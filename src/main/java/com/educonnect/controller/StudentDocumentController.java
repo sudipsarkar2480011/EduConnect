49ec2e7c6b09395da77e423f7f5201379f0ee8c5
Sanchita Das <Sanchita.Das@cognizant.com>
2026-02-23 12:24:17 +0530

    normal commit

--- src/main/java/com/educonnect/controller/StudentDocumentController.java
+++ src/main/java/com/educonnect/controller/StudentDocumentController.java
@@ -19,7 +19,6 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/api/doc")
 public class StudentDocumentController {
-
     private final StudentDocumentService studentDocumentService;
 
     @PostMapping(path = "/upload", consumes = "multipart/form-data")
