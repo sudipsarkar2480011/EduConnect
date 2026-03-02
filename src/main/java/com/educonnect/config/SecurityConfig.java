ed1988de217d24ed476c6aec4477017c73b17a9c
santadip-rudra <Santadip.Rudra@cognizant.com>
2026-03-02 14:16:36 +0530

    added debuggin to spring security

--- src/main/java/com/educonnect/config/SecurityConfig.java
+++ src/main/java/com/educonnect/config/SecurityConfig.java
@@ -16,7 +16,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
-@EnableWebSecurity
+@EnableWebSecurity(debug = true)
 @RequiredArgsConstructor
 public class SecurityConfig {
     private final EduconnectUserDetailsService educonnectUserDetailsService;
