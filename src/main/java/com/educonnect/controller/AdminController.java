1bcd7af3785c33c19f9115254a4ba6c3124f4a30
A B Ramya <2479308@cognizant.com>
2026-03-02 10:37:43 +0530

    Adding AdminController.java

---
+++ src/main/java/com/educonnect/controller/AdminController.java
@@ -0,0 +1,18 @@
+package com.educonnect.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * @author santadiprudra
+ * @version 1.0
+ * @since 1.0
+ * */
+@RestController
+@RequestMapping("/v1/api/admin")
+public class AdminController {
+
+
+}
