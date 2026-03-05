a7a0e36c78cd97772d31c8ea6af2cc91f49a5d74
Sanchita Das <Sanchita.Das@cognizant.com>
2026-03-06 00:07:06 +0530

    MINOR FIX

--- src/main/java/com/educonnect/service/implementation/AdminServiceImpl.java
+++ src/main/java/com/educonnect/service/implementation/AdminServiceImpl.java
@@ -5,11 +5,12 @@ import com.educonnect.model.user.Admin;
 import com.educonnect.repo.AdminRepo;
 import com.educonnect.service.contract.AdminService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.UUID;
 
-
+@Service
 public class AdminServiceImpl implements AdminService {
 
     @Autowired
