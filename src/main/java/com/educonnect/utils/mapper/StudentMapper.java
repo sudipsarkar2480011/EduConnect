75f919b72ed76a0f4480b2e38b2af420c87aa687
santadip-rudra <Santadip.Rudra@cognizant.com>
2026-03-09 23:31:40 +0530

    added javadoc for multiple files

--- src/main/java/com/educonnect/utils/mapper/StudentMapper.java
+++ src/main/java/com/educonnect/utils/mapper/StudentMapper.java
@@ -1,12 +1,12 @@
 package com.educonnect.utils.mapper;
 
+import com.educonnect.dto.user.UserRequestDTO;
 import com.educonnect.model.user.Student;
 import com.educonnect.dto.student.StudentResponse;
 import org.springframework.stereotype.Component;
 
 @Component
-public class StudentMapper {
-
+public class StudentMapper implements Mapper<Student, UserRequestDTO,StudentResponse>{
     public StudentResponse toResponse(Student s) {
         if (s == null) return null;
         return new StudentResponse(
@@ -16,6 +16,24 @@ public class StudentMapper {
                 s.getRole(),
                 s.isActive(),
                 s.getEnrollments()
+        );
+    }
+
+    @Override
+    public Student toEntity(UserRequestDTO requestDTO) {
+        return null;
+    }
+
+    @Override
+    public StudentResponse toResponseDT(Student entity) {
+        if (entity == null) return null;
+        return new StudentResponse(
+                entity.getUserId(),     // inherited from User
+                entity.getFullName(),
+                entity.getEmail(),
+                entity.getRole(),
+                entity.isActive(),
+                entity.getEnrollments()
         );
     }
 }
