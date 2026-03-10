f8a869ba8c0faa79a2a92b890104805004ca87cd
santadip-rudra <Santadip.Rudra@cognizant.com>
2026-03-10 21:28:56 +0530

    added admin role in spring security

--- src/main/java/com/educonnect/config/SecurityConfig.java
+++ src/main/java/com/educonnect/config/SecurityConfig.java
@@ -38,17 +38,13 @@ public class SecurityConfig {
                                 "/swagger-ui/**"
 
                 ).permitAll()
-                       // .requestMatchers("/v1/api/result/**").hasAnyRole("TEACHER","STUDENT") // NOT WORKING
-                        .requestMatchers("/v1/api/result/**").hasRole("TEACHER")
-                        //.requestMatchers("/v1/api/**").hasRole("ADMIN")
-                        .requestMatchers("/v1/api/teachers/**").hasRole("TEACHER")
+                        .requestMatchers("/v1/api/result/**","/v1/api/teachers/**").hasRole("TEACHER")
                         .requestMatchers("/v1/api/parent/**").hasRole("PARENT")
                         .requestMatchers("/v1/api/assessment/create").hasRole("TEACHER")
-                        .requestMatchers("/v1/api/assessment/submit").hasRole("STUDENT")
-                        .requestMatchers(
-                                "/v1/api/student/**",
-                                "/v1/api/attendance/**",
-                                "/v1/api/doc/**").hasRole("STUDENT")
+                        .requestMatchers("/v1/api/student/**", "/v1/api/attendance/**", "/v1/api/doc/**", "/v1/api/assessment/submit")
+                        .hasRole("STUDENT")
+
+                        .requestMatchers("/v1/api/audits/**").hasRole("ADMIN")
 
                         .anyRequest().authenticated())
                         .sessionManagement(
