8ed0cf692d195b43dbca6fbcb90ae9b42ced4842
sudipsarkar2480011 <Sudp.Sarkar@cognizant.com>
2026-03-12 09:27:01 +0530

    removed println statements

--- src/main/java/com/educonnect/service/implementation/course/CourseVideoImpl.java
+++ src/main/java/com/educonnect/service/implementation/course/CourseVideoImpl.java
@@ -155,7 +155,6 @@ public class CourseVideoImpl implements CourseVideoService {
            log.error(e.getMessage());
            throw e;
        }finally {
-            System.out.println("finally");
             if(tempFile != null) {
                 Files.deleteIfExists(tempFile.toPath());
             }
@@ -248,7 +247,6 @@ public class CourseVideoImpl implements CourseVideoService {
             Files.deleteIfExists(path);
         } catch (IOException e) {
             log.error(e.getMessage());
-            System.out.println(e.getMessage());
             throw new IOException(e);
         }
         courseModuleRepo.deleteById(video.getModuleId());
@@ -330,15 +328,6 @@ public class CourseVideoImpl implements CourseVideoService {
 
 
 
-            System.out.println();
-            System.out.println();
-            System.out.println();
-            System.out.println("duration "+ duration);
-            System.out.println("video.getDuration() "+ video.getDuration());
-            System.out.println("course.getDuration() "+ course.getDuration());
-            System.out.println();
-            System.out.println();;
-
             course.setDuration(
                     course.getDuration() != null ?
                             course.getDuration() - video.getDuration() + duration : 0
@@ -353,7 +342,6 @@ public class CourseVideoImpl implements CourseVideoService {
             courseRepo.save(course);
 
             log.info("video deleted successfully course table");
-            System.out.println("video deleted successfully from course table");
             return video;
 
         } catch (Exception e) {
